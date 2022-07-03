4c7a96f1a1e6d42ea595afd6ba5cf626db20bda1
danylokosenko <danylo.kosenko@yalantis.net>
2022-07-03 15:45:48 +0300

    session_store.rb configured not only for prod

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,6 @@
-Rails.application.config.session_store :cookie_store, key: "_generator_app",
-                                       domain: "dnks-team-generator-app.herokuapp.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_generator_app",
+  domain: "dnks-team-generator-app.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_generator_app"
+end
